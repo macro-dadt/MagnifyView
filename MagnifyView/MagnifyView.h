b449a561f0378d429f66379f5519d54048e812c5
macro.dat <macro.dat@trim-inc.com>
2018-06-07 23:03:18 +0900

    initial commit

---
+++ MagnifyView/MagnifyView.h
@@ -0,0 +1,19 @@
+//
+//  MagnifyView.h
+//  MagnifyView
+//
+//  Created by Do Thanh Dat on 2018/06/07.
+//  Copyright © 2018 DT Dat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MagnifyView.
+FOUNDATION_EXPORT double MagnifyViewVersionNumber;
+
+//! Project version string for MagnifyView.
+FOUNDATION_EXPORT const unsigned char MagnifyViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagnifyView/PublicHeader.h>
+
+
